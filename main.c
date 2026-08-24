ff9bd9253f9dea6cfb401918804b9f5ec99dfc00
ricahmenarciso <ricahmen@gmail.com>
2026-08-24 21:12:02 +0800

    Add files via upload
    Submit Activity 02.

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-   printf("Name:Narciso, Ricahme\n");
-   printf ("Course: BSIT\n");
-   printf ("Section: 1-A\n");
-   return 0;
+int num1 = 8;
+    int num2 = 12;
+    int sum = num1 + num2; // Calculates 8 + 12 = 20
+ printf("Enter first number: %d\n", num1);
+    printf("Enter second number: %d\n", num2);
+    printf("Sum: %d\n", sum);
+    return 0;
 }
