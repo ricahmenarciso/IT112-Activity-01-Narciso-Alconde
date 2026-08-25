665e9af08dd742fb454837ffd1a0aab385ff3c43
ricahmenarciso <ricahmen@gmail.com>
2026-08-25 08:20:38 +0800

    Add files via upload
    Submit Activity 02

---
+++ addition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int num1 = 8;
+    int num2 = 12;
+    int sum = num1 + num2;
+
+
+    printf("Enter first number: %d\n", num1);
+    printf("Enter second number: %d\n", num2);
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
